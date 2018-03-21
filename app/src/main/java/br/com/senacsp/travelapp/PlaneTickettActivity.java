f5543d4525bd34d31ec27e36e3ecbc10eb6dfa28
WELLINGTON DE SOUSA ALMEIDA <wellington.salmeida7@senacsp.edu.br>
2018-03-21 12:20:30 -0300

    Initial commit

---
+++ app/src/main/java/br/com/senacsp/travelapp/PlaneTickettActivity.java
@@ -0,0 +1,13 @@
+package br.com.senacsp.travelapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PlaneTickettActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_plane_tickett);
+    }
+}
---
+++ app/src/main/java/br/com/senacsp/travelapp/SingUp_Activity.java
@@ -0,0 +1,13 @@
+package br.com.senacsp.travelapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SingUp_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_sing_up);
+    }
+}
